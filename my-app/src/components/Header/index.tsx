dfa9f563b5729339cffdcf853a2b075d488c86fe
alanguilhermeM <alanguilherme13@outlook.com>
2025-02-12 12:02:07 -0300

    Iniciando Projeto

---
+++ my-app/src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+export default function Header() {
+  return (
+    <header className="flex justify-center w-full">
+      <nav>
+        <ul>
+          <li>
+            <a href="/projetos">Projetos</a>
+          </li>
+          <li>
+            <a href="/skills">Skills</a>
+          </li>
+          <li>
+            <a href="/sobre">Sobre Mim</a>
+          </li>
+          <li>
+            <a href="/contato">Contato</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
