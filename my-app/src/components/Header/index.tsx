ec189f38ef2fb8bf7ca1b26c02251c78be624568
alanguilhermeM <alanguilherme13@outlook.com>
2025-02-15 07:26:00 -0300

    Header Basic Component

--- my-app/src/components/Header/index.tsx
+++ my-app/src/components/Header/index.tsx
@@ -1,22 +1,29 @@
+import Image from "next/image"
+
 export default function Header() {
   return (
     <header className="flex justify-center w-full">
-      <nav>
-        <ul>
-          <li>
-            <a href="/projetos">Projetos</a>
-          </li>
-          <li>
-            <a href="/skills">Skills</a>
-          </li>
-          <li>
-            <a href="/sobre">Sobre Mim</a>
-          </li>
-          <li>
-            <a href="/contato">Contato</a>
-          </li>
-        </ul>
-      </nav>
+      <section className="flex justify-between w-[60%] py-10">
+        <article className="w-[50%]">
+          <Image src='/logo.png' alt="Logo" width={80} height={80} className="hover:scale-105 transition duration-500 ease-in-out" />
+        </article>
+        <nav className="flex items-center w-[50%]">
+            <ul className="flex justify-around w-full items-center text-black text-lg">
+              <li className="hover:text-[#7A63FF] transition duration-200 ease-in-out">
+                  <a href="/projetos">Projetos</a>
+              </li>
+              <li className="hover:text-[#7A63FF] transition duration-200 ease-in-out">
+                  <a href="/skills">Skills</a>
+              </li>
+              <li className="hover:text-[#7A63FF] transition duration-200 ease-in-out">
+                  <a href="/sobre">Sobre Mim</a>
+              </li>
+              <li className="text-[#7A63FF] hover:text-white hover:bg-[#7A63FF] transition duration-200 ease-in-out border-2 border-[#7A63FF] rounded-full">
+                  <a className="block w-full h-full p-3 text-center" href="/contato">Entre em Contato</a>
+              </li>
+            </ul>
+        </nav>
+      </section>
     </header>
   );
 }
