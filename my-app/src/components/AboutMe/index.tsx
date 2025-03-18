2809a90b3916708c2223037fb8513bdb03ca0f89
alanguilhermeM <alanguilherme13@outlook.com>
2025-03-18 08:38:42 -0300

    fix: media querie about

--- my-app/src/components/AboutMe/index.tsx
+++ my-app/src/components/AboutMe/index.tsx
@@ -7,13 +7,18 @@ export default function AboutMe() {
   const intl = useIntl();
 
   return (
-    <main id='sobre' className="flex flex-col justify-center items-center w-full h-fit py-20">
-      <article className="flex-col justify-center content-center justify-items-center max-ss:h-[600px] md:h-[800px] xl:h-[600px] max-sm:h-[800px] bg-[#6E07F3]">
-        <section className="text-center text-white max-ss:w-[80%] md:w-[70%] lg:w-[50%] h-[70%] ss:w-[85%]">
-          <h2 className="w-full font-zain font-bold max-ss:text-2xl md:text-4xl ss:text-4xl pb-4">{intl.formatMessage({ id: "about" })}</h2>
-          <span className="w-full font-zain font-medium md:text-3xl ss:text-2xl pt-4">{intl.formatMessage({ id: "aboutDescription" })}</span>
+    <main id="sobre" className="flex flex-col justify-center items-center w-full h-fit py-20">
+      <article className="flex flex-col items-center justify-center w-full max-lg:h-[600px] lg:h-[500px] xl:h-[450px] bg-[#6E07F3]">
+        <section className="flex flex-col items-center text-center text-white max-ss:w-[80%] md:w-[70%] lg:w-[50%] ss:w-[85%]">
+          <h2 className="w-full font-zain font-bold max-ss:text-2xl md:text-4xl ss:text-4xl pb-4">
+            {intl.formatMessage({ id: "about" })}
+          </h2>
+          <span className="w-full font-zain font-medium md:text-3xl ss:text-2xl pt-4">
+            {intl.formatMessage({ id: "aboutDescription" })}
+          </span>
         </section>
       </article>
+      <div className="w-full h-[200px] bg-[#6e07f3]" />
       <Skills />
     </main>
   );
